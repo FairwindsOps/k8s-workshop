26dba0e1f05eda2dfd279cf6a39ef84cebe2ff3f
Andrew Suderman <andrew@sudermanjr.com>
2019-10-08 23:46:35 -0600

    Fixign script

--- complete/load.sh
+++ complete/load.sh
@@ -2,6 +2,7 @@
 
 if [ -z $1 ]; then
     echo "You need to specify an address to target"
+    exit 1
 fi
 
 command -v k6 >/dev/null 2>&1 || { echo >&2 "I require k6 but it's not installed.  Aborting."; exit 1; }
