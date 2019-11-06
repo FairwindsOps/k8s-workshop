cd9d0975cb9ed35d109435990a625824d6ade27d
Andrew Suderman <andrew@sudermanjr.com>
2019-11-06 13:13:43 -0700

    Adding metrics server install

--- eks/scripts/create-cluster.sh
+++ eks/scripts/create-cluster.sh
@@ -26,4 +26,13 @@ if [ -z "${CLUSTERID}" ]; then
 fi
 
 eksctl create cluster --name $CLUSTERID
+
+#Install metrics-server
+DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/kubernetes-sigs/metrics-server/releases/latest" | jq -r .tarball_url)
+DOWNLOAD_VERSION=$(grep -o '[^/v]*$' <<< $DOWNLOAD_URL)
+curl -Ls $DOWNLOAD_URL -o metrics-server-$DOWNLOAD_VERSION.tar.gz
+mkdir metrics-server-$DOWNLOAD_VERSION
+tar -xzf metrics-server-$DOWNLOAD_VERSION.tar.gz --directory metrics-server-$DOWNLOAD_VERSION --strip-components 1
+kubectl apply -f metrics-server-$DOWNLOAD_VERSION/deploy/1.8+/
+
 kubectl get nodes
