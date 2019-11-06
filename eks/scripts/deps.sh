a011b3202f6038edd3cc81c87cb8c7fdc20dc8ef
Andrew Suderman <andrew@sudermanjr.com>
2019-11-06 10:06:24 -0700

    Update kubectl

--- eks/scripts/deps.sh
+++ eks/scripts/deps.sh
@@ -1,22 +1,23 @@
 #! /usr/bin/env bash
 #
-# adds dependencies to cloud9 instance for 
+# adds dependencies to cloud9 instance for
 # completing the ReactiveOps EKS 101 training
 #
 
 #- VARIABLES -#
 ARCH="amd64"
-KUBECTL_VERSION="1.10.3/2018-07-26"
+KUBECTL_VERSION="1.14.8"
 
 
 #- PROCEDURAL -#
 
 # install kubectl
 mkdir -p ~/.kube
-sudo curl --silent --location \
-    -o /usr/bin/kubectl \
-    "https://amazon-eks.s3-us-west-2.amazonaws.com/${KUBECTL_VERSION}/bin/linux/${ARCH}/kubectl"
-sudo chmod +x /usr/bin/kubectl
+
+# Install kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/${KUBECTL_VERSION}/bin/linux/amd64/kubectl \
+  && chmod +x ./kubectl \
+  && mv ./kubectl /usr/local/bin/kubectl
 
 # install aws-iam-authenticator
 go get -u -v github.com/kubernetes-sigs/aws-iam-authenticator/cmd/aws-iam-authenticator
