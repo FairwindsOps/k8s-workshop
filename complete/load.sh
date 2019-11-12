e540287b6dc1f3eaedac2835b5880cc54c619b62
ej@fairwinds.com <ej@fairwinds.com>
2019-11-12 12:58:40 -0800

    fixing awk from load gen, adding protocol

--- complete/load.sh
+++ complete/load.sh
@@ -2,13 +2,13 @@
 
 command -v k6 >/dev/null 2>&1 || { echo >&2 "I require k6 but it's not installed.  Aborting."; exit 1; }
 
-elb=$(kubectl get svc -n k8s-workshop | grep LoadBalancer | awk '{print $3}')
+elb=$(kubectl get svc -n k8s-workshop | grep LoadBalancer | awk '{print $4}')
 
 if [ -z "$elb" ]; then
     echo "Could not find a service to target in k8s-workshop!"
     exit 1
 fi
 echo $elb
-export TARGET=$elb
+export TARGET="http://${elb}"
 
 k6 run load.js --vus=5 --duration=0 -q --max=100
